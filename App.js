b6551af0ac666caf3da39d14c9e7cd32f48213bd
sarikagowtham <sarikagwthm2994@gmail.com>
2018-12-04 17:44:38 +0530

    files added

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
-import React , {Component} from 'react';
+import React from  'react';
 import {Platform,Text,Button,View} from 'react-native';
 import {createStackNavigator,createAppContainer} from 'react-navigation';
 import Main from './android/app/source/main';
-import Home from './android/app/source/home';
+import SignUp from './android/app/source/signup'
+import Login from './android/app/source/login'
+import Loading from './android/app/source/loading'
 
 
-const AppstackNavigator=createStackNavigator({
-   main: Main,
-   
-
-  home1:Home
+const app=createStackNavigator({
   
-   
+ Loading: Loading,
+ Signup:SignUp,
+ Login: Login,
+ Main: Main
+},
+{
+  initialRouteName: 'Loading'
+}
   
-})
-export default createAppContainer(AppstackNavigator);
+)
+export default createAppContainer(app);
--- android/app/source/main.js
+++ android/app/source/main.js
@@ -1,19 +1,28 @@
 import React , {Component} from 'react';
 import {Platform,TextInput,Button,View} from 'react-native';
-
+import firebase from 'react-native-firebase'
  class Main extends Component{
-    render()
-    {
-        return(
-            <View>
-                <TextInput placeholder="E-mail"/>
-                <TextInput placeholder="Password"/>
-                <Button title="Login"
-                 onPress={()=>this.props.navigation.navigate('home1')}
-
-                />
-            </View>
-        );
+    state = { currentUser: null }
+    componentDidMount() {
+        const { currentUser } = firebase.auth()
+        this.setState({ currentUser })
     }
+render() {
+    const { currentUser } = this.state
+return (
+      <View style={styles.container}>
+        <Text>
+          Hi {currentUser && currentUser.email}!
+        </Text>
+      </View>
+    )
+  }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 export default Main;
